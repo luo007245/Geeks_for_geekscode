b0c38cdaff2ee7ffced50dccc66f065de58ea164
Ankush Gaur <MY_NAME@example.com>
2023-07-01 17:17:08 +0530

    1st july code

---
+++ Number of 1 Bits/Solution.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+ 
+public setBits(int N){
+
+    int count  = 0
+    for(int i = 31; i >=0; i--)
+    {
+      int bit =(N>>i) & 1;
+      if (bit ==1)
+      {
+        count++;
+      }
+      
+    }
+    return count;
+    
+}
+};
+
